82c3e18b0a45b66539de57597579dc2b925f640f
zlw9161 <lwzhang9161@126.com>
2020-03-03 14:00:52 +0800

    first commit

---
+++ command_evaluate_dcase2019.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -x #echo on
+export PYTHONPATH=$PYTHONPATH:$1
+
+command_file=`basename "$0"`
+script_file=evaluate.py
+gpu=0
+data=/mnt/data/datasets/dcase2019/dev/task1a
+model=resnet18_senot_128_80
+model_path=/mnt/data/SeNot-Net/log_dcase_resnet18_senot_128_80_8_sn4_train/model-100.ckpt
+num_threads=6
+num_segs=8
+timebins=80
+freqbins=128
+num_classes=10
+sn=4
+fcn=0
+dump_dir=log_dcase_resnet18_senot_128_80_8_sn${sn}_eval
+log_file=$dump_dir.txt
+
+
+python3 $script_file \
+    --gpu $gpu \
+    --data $data \
+    --model $model \
+    --model_path $model_path \
+    --num_threads $num_threads \
+    --num_segs $num_segs \
+    --timebins $timebins \
+    --freqbins $freqbins \
+    --num_classes $num_classes \
+    --sn $sn \
+    --dump_dir $dump_dir \
+    --fcn $fcn \
+    --command_file $command_file \
+    > $log_file 2>&1 &
